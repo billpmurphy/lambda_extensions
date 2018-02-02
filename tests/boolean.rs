1b97ff4c828c7b14f1dc3b487d59e83d705e780c
Bill Murphy <billpmurphy92@gmail.com>
2018-02-01 22:26:23 -0500

    update boolean convert test

--- tests/boolean.rs
+++ tests/boolean.rs
@@ -5,9 +5,9 @@ use lambda_extensions::*;
 use lambda_calculus::data::boolean;
 
 #[test]
-fn test_convert_bool_from_term() {
-    assert_eq!(abs(Var(1)).try_from(), None);
-    assert_eq!(abs!(3, Var(0)).try_from(), None);
-    assert_eq!(boolean::fls().try_from(), Some(false));
-    assert_eq!(boolean::tru().try_from(), Some(true));
+fn test_convert_term_to_bool() {
+    assert_eq!(abs(Var(1)).try_from() as Option<bool>, None);
+    assert_eq!(abs!(3, Var(0)).try_from() as Option<bool>, None);
+    assert_eq!(boolean::fls().try_from() as Option<bool>, Some(false));
+    assert_eq!(boolean::tru().try_from() as Option<bool>, Some(true));
 }

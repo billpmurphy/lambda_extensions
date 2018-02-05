e09b338ec39bc7554fe80ae5558908db3c4d5b7e
Bill Murphy <billpmurphy92@gmail.com>
2018-02-05 12:37:09 -0500

    add parse_num

--- src/data/string.rs
+++ src/data/string.rs
@@ -1,6 +1,50 @@
 //! Church-encoded string represented as a pair-list of Church-encoded characters
 
 use lambda_calculus::*;
+use lambda_calculus::data::num::church;
+use lambda_calculus::data::list::pair as pair_list;
+use lambda_calculus::data::option;
+use data::char;
+use data::option::foldm;
+
+/// Applied to a Church-encoded string it produces an `Option` containing the value of the string
+/// parsed as an unsighed integer, represented as a Church-encoded numeral.
+///
+/// PARSE_NUM ≡ λs.IS_NUL s NONE (FOLDM (λac.AND_THEN (TO_DIGIT c) (λx.SOME (ADD x (MUL a TEN)))) ZERO s)
+///           ≡ λ IS_NUL 1 NONE (FOLDM (λ λ AND_THEN (TO_DIGIT 1) (λ SOME (ADD 1 (MUL 3 TEN)))) ZERO 1)
+///
+/// # Example
+/// ```
+/// use lambda_extensions::*;
+/// use lambda_extensions::data::string::parse_num;
+///
+/// assert_eq!(beta(app(parse_num(), "4".into_church()), NOR, 0), Some(4).into_church());
+/// assert_eq!(beta(app(parse_num(), "10".into_church()), NOR, 0), Some(10).into_church());
+/// ```
+pub fn parse_num() -> Term {
+    abs(app!(
+        pair_list::is_nil(),
+        Var(1),
+        option::none(),
+        app!(
+            foldm(),
+            abs!(2, app!(
+                option::and_then(),
+                app(char::to_digit(), Var(1)),
+                abs(app(
+                    option::some(),
+                    app!(
+                        church::add(),
+                        Var(1),
+                        app!(church::mul(), Var(3), 10.into_church())
+                    )
+                ))
+            )),
+            church::zero(),
+            Var(1)
+        )
+    ))
+}
 
 /// Conversion from a Rust `&str` to a Church-encoded character.
 pub trait IntoChurchString {
--- tests/string.rs
+++ tests/string.rs
@@ -1,6 +1,8 @@
 extern crate lambda_extensions;
 
 use lambda_extensions::*;
+use lambda_extensions::data::string::*;
+use lambda_extensions::utils::assert_lc;
 
 fn into_church_pairlist(v: Vec<char>) -> Term {
     v.iter().map(|c| c.into_church()).collect::<Vec<Term>>().into_pair_list()
@@ -11,4 +13,23 @@ fn test_string_convert() {
     assert_eq!("".into_church(), into_church_pairlist(vec![]));
     assert_eq!("a".into_church(), into_church_pairlist(vec!['a']));
     assert_eq!("abc".into_church(), into_church_pairlist(vec!['a', 'b', 'c']));
+}
+
+#[test]
+#[ignore]
+fn test_parse_num() {
+    let none: Option<usize> = None;
+    assert_lc(app(parse_num(), "".into_church()), none.into_church());
+    assert_lc(app(parse_num(), "a".into_church()), none.into_church());
+    assert_lc(app(parse_num(), "1a".into_church()), none.into_church());
+    assert_lc(app(parse_num(), "a1".into_church()), none.into_church());
+    assert_lc(app(parse_num(), "1a1".into_church()), none.into_church());
+    assert_lc(app(parse_num(), "1 1".into_church()), none.into_church());
+    assert_lc(app(parse_num(), "1".into_church()), Some(1).into_church());
+    assert_lc(app(parse_num(), "01".into_church()), Some(1).into_church());
+    assert_lc(app(parse_num(), "0001".into_church()), Some(1).into_church());
+    assert_lc(app(parse_num(), "0010".into_church()), Some(10).into_church());
+    assert_lc(app(parse_num(), "101".into_church()), Some(101).into_church());
+    assert_lc(app(parse_num(), "200".into_church()), Some(200).into_church());
+    assert_lc(app(parse_num(), "234".into_church()), Some(234).into_church());
 }
